a1487ab184474c7d5d2ee0671005a5963b32ae34
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-07-19 00:29:01 +0530

    Whilist done

--- store/components/ProductCard.tsx
+++ store/components/ProductCard.tsx
@@ -1,11 +1,65 @@
-import { HeartIcon } from "lucide-react";
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+  const router = useRouter()
+  const { user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const getUser = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/users");
+      const data = await res.json();
+      setSignedInUser(data);
+      setIsLiked(data.wishlist.includes(product._id));
+      setLoading(false);
+    } catch (error) {
+      console.log("[users_GET]", error);
+    }
+  };
+
+  useEffect(() => {
+    if(user) {
+      getUser()
+    }
+  }, [user]);
+
+  const handleLike = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+
+    try {
+      if (!user) {
+        router.push('/sign-in')
+        return
+      } else {
+        setLoading(true)
+        const res = await fetch('/api/users/wishlist', {
+          method: "POST"
+          body: JSON.stringify({productId: product._id})
+        })
+        const updatedUser = await res.json()
+        setSignedInUser(updatedUser)
+        setIsLiked(updatedUser.wishlist.includes(product._id))
+      }
+    } catch (error) {
+      console.log("[wishlist_POST]",error)
+    }
+  }
+
   return (
-    <Link href={`/products/${product._id}`} className="w-[220px] flex flex-col gap-2">
+    <Link
+      href={`/products/${product._id}`}
+      className="w-[220px] flex flex-col gap-2"
+    >
       <Image
         src={product.media[0]}
         alt="product"
@@ -19,8 +73,8 @@ const ProductCard = ({ product }: { product: ProductType }) => {
       </div>
       <div className="flex items-center justify-between">
         <p className="text-body-bold">LKR {product.price}</p>
-        <button>
-          <HeartIcon />
+        <button onClick={handleLike}>
+          <Heart fill={`${isLiked ? "red" : "white"}`} />
         </button>
       </div>
     </Link>
